04951e2be00822d17777b18797ca6cc8b55a123e
Oscar <oscar.juarezotz@gmail.com>
2026-02-14 12:05:34 -0600

    Proyecto MAUI añadido

---
+++ Aplicacion_Artista/Aplicacion_Artista/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Aplicacion_Artista
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Aplicacion_Artista/Aplicacion_Artista/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Aplicacion_Artista
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

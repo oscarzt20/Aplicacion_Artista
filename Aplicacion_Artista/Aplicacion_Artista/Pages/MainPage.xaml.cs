bb8d53a1fc8287d041b7323e45ff28395a860a76
Garlink27 <bgael3512@gmail.com>
2026-02-27 08:49:18 -0600

    Cambios de colores

--- Aplicacion_Artista/Aplicacion_Artista/Pages/MainPage.xaml.cs
+++ Aplicacion_Artista/Aplicacion_Artista/Pages/MainPage.xaml.cs
@@ -13,12 +13,16 @@
             {
                 Shell.Current.Resources["BackgroundMain"] = Color.FromHex("#90d5ff");
                 Shell.Current.Resources["Text"] = Colors.Black;
+                Shell.Current.Resources["AccentColor"] = Colors.Black;
+                Shell.Current.Resources["SecondaryBackgorund"] = Color.FromHex("#d7c2b0");
 
                 return;
             }
 
             Shell.Current.Resources["BackgroundMain"] = Color.FromHex("#1a2634");
             Shell.Current.Resources["Text"] = Colors.White;
+            Shell.Current.Resources["AccentColor"] = Color.FromHex("#00d2ff");
+            Shell.Current.Resources["SecondaryBackgorund"] = Color.FromHex("#283d4f");
 
             return;
         }
